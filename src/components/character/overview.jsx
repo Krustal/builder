a294a3bbffa8db514603abe85867606f80313351
Byron McMullen <byronsm@gmail.com>
2016-03-11 08:42:38 -0800

    overview section css-modules

--- src/components/character/overview.jsx
+++ src/components/character/overview.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import components from '../../styles/components.css';
+import inputStyle from '../../styles/input.css';
 
 export default class CharacterOverview extends React.Component {
   render() {
     return (
       <div className={components.characterOverview}>
-        <label className="basic">name<input type="text" /></label>
-        <label className="basic">race<input type="text" /></label>
-        <label className="basic">class<input type="text" /></label>
-        <label className="basic">level<input type="text" /></label>
+        <label className="basic">name<input className={inputStyle.simpleText} type="text" /></label>
+        <label className="basic">race<input className={inputStyle.simpleText} /></label>
+        <label className="basic">class<input className={inputStyle.simpleText} /></label>
+        <label className="basic">level<input className={inputStyle.simpleText} /></label>
       </div>
     );
   }
