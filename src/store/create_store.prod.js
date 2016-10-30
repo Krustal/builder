c4d2338d8c956823392fed02ecc830d2b162a4fe
Byron McMullen <byronsm@gmail.com>
2016-10-29 20:48:55 -0700

    production store builder updated

--- src/store/create_store.prod.js
+++ src/store/create_store.prod.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Character from '../lib/thirteenth_age_character.js';
 import characterReducer from '../reducers/character.js';
+import CharacterInterface from '../lib/character_interface';
 
-const initialCharacter = Character.create();
+const initialCharacter = new CharacterInterface(Character.create());
 
-const reducer = combineReducers({ character: characterReducer(initialCharacter) });
+const reducer = combineReducers({ character: characterReducer });
 
 export default () => createStore(
   reducer,
-  applyMiddleware(thunk.withExtraArgument({ character: initialCharacter }))
+  applyMiddleware(thunk.withExtraArgument({ characterInterface: initialCharacter }))
 );
